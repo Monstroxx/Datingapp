92594a3c414805eeb96002c752345c9c3db8ea3d
TagerTi <101815433+TagerTi@users.noreply.github.com>
2025-10-01 09:57:34 +0200

    Utility Folder and .env file

---
+++ BananaLove/Utility/.env.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaLove.Utility
+{
+    static class Env
+    {
+        public static string DatabaseServerIP = "127.0.0.1"; // set your own mysql server ip here
+        public static string DatabaseName = "BananaLove"; // set your own database name here
+        public static string DatabaseServerProfilename = "root"; // set your own mysql username here
+        public static string DatabaseServerPassword = ""; // set your own password here
+    }
+}
