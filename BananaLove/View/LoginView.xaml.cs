46164065fe243cf1ecfa91dc69c8574ef302c44c
TagerTi <101815433+TagerTi@users.noreply.github.com>
2025-10-01 10:17:15 +0200

    autoload env at login screen

--- BananaLove/View/LoginView.xaml.cs
+++ BananaLove/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BananaLove.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace BananaLove.View
         public LoginView()
         {
             InitializeComponent();
+            EnvHandler.LoadEnvs(".env");
         }
 
         public void Window_MouseDown(object sender, MouseButtonEventArgs e)
