90c955935ead0ccb57fd1f396a7c8b756f919ed6
TagerTi <101815433+TagerTi@users.noreply.github.com>
2025-11-04 09:18:44 +0100

    MySQL Connector

---
+++ BananaLove/Utility/DBHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace BananaLove.Utility
+{
+    public static class DBHandler
+    {
+        
+
+        public enum LoginStates
+        {
+            NewUser,
+            ExistingUser,
+            Error,
+        }
+
+        public static Login TryLogin(String email, String password)
+        {
+            return new Login(0,0,LoginStates.Error);
+        }
+
+        public static bool SaveLogin(  )
+        {
+            return false;
+        }
+
+    }
+
+    public class Login
+    {
+        int UserID, LoginID;
+        DBHandler.LoginStates State;
+
+        public Login(int userID, int loginID, DBHandler.LoginStates state)
+        {
+            UserID = userID;
+            LoginID = loginID;
+            State = state;
+        }
+    }
+}
--- BananaLove/View/LoginView.xaml.cs
+++ BananaLove/View/LoginView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BananaLove.Utility;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
