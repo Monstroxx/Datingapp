a78a18134ebbf5a9582af55dafaeb9891e7242a1
Monstroxx <152517394+Monstroxx@users.noreply.github.com>
2025-11-05 08:17:02 +0100

    debug.log wirt beim startup gelöscht

--- BananaLove/App.xaml.cs
+++ BananaLove/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace BananaLove
@@ -9,6 +10,15 @@ namespace BananaLove
     /// </summary>
     public partial class App : Application
     {
+        //delete debug.log on start
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (File.Exists("debug.log"))
+            {
+                File.Delete("debug.log");
+            }
+        }
     }
 
 }
